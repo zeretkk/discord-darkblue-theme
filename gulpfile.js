6f4da56c87dbc6b2263b4e2ec57ab8f0802a4f2e
zeretkk <kirill03192@gmail.com>
2021-08-04 18:39:11 +0300

    gulp autobuild function

--- gulpfile.js
+++ gulpfile.js
@@ -11,15 +11,20 @@ function cleanDist(){
 
 function buildCss(){
     return gulp.src('src/style.scss')
-    .pipe(sass({}))
+    .pipe(sass())
     .pipe(gulp.dest('dist'))
     .pipe(csso())
     .pipe(rename({suffix:'.min'}))
     .pipe(gulp.dest('dist'))
 }
-
+function autoBuild(){
+    gulp.watch(['src/**/*.scss'], buildCss)
+}
 
 const build = gulp.series(cleanDist, buildCss)
+const watch = gulp.parallel(build, autoBuild)
 
 exports.build = build
 exports.clean = gulp.series(cleanDist)
+exports.watch = watch
+exports.default = watch
